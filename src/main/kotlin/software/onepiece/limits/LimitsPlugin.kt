3350e423badee9709e12c76c813344203a792adc
Jendrik Johannes <jendrik@gradle.com>
2018-06-09 17:56:40 +0200

    Fixes

--- src/main/kotlin/software/onepiece/limits/LimitsPlugin.kt
+++ src/main/kotlin/software/onepiece/limits/LimitsPlugin.kt
@@ -85,7 +85,7 @@ class LimitsPlugin: Plugin<Project> {
             when(spec) {
                 is CoordinateSpec -> setOf(spec)
                 is Coordinates2Spec -> setOf(spec, spec.xType, spec.yType)
-                is ContainerSpec -> setOf(spec) + collectSpecs(spec.containedType) + spec.additionalContainedTypes.map { collectSpecs(it) }.flatten() + collectSpecs(spec.coordinatesType) + collectSpecs(spec.containedLocation)
+                is ContainerSpec -> setOf(spec) + collectSpecs(spec.containedType) + spec.additionalContainedTypes.map { collectSpecs(it) }.flatten() + collectSpecs(spec.coordinatesType) + collectSpecs(spec.containedLocation) + spec.attributes.map { collectSpecs(it) }.flatten()
                 is SuperContainerSpec -> setOf(spec)
                 is ChainOfCoordinates -> setOf(spec) + spec.components
                 else -> setOf()
