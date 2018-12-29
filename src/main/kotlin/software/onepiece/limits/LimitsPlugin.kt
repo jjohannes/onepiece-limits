2c051344ac99eb0f90508797f6993462d324dfa8
Jendrik Johannes <jendrik@gradle.com>
2018-12-29 10:12:27 +0100

    Small improvements and preparations for Gradle 5.1

--- src/main/kotlin/software/onepiece/limits/LimitsPlugin.kt
+++ src/main/kotlin/software/onepiece/limits/LimitsPlugin.kt
@@ -5,6 +5,7 @@ import org.gradle.api.Project
 import org.gradle.api.internal.HasConvention
 import java.io.File
 import org.gradle.api.tasks.SourceSetContainer
+import org.gradle.kotlin.dsl.project
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import software.onepiece.limits.spec.*
 
@@ -31,7 +32,7 @@ class LimitsPlugin: Plugin<Project> {
                 val generatedSrcFolder = File(sub.buildDir, "generated-src")
 
                 sub.plugins.apply("org.jetbrains.kotlin.jvm")
-                sub.dependencies.add("compile", "org.jetbrains.kotlin:kotlin-stdlib-jre8:1.2.21")
+                sub.dependencies.add("compile", "org.jetbrains.kotlin:kotlin-stdlib:1.3.0")
 
                 val sourceSets = sub.properties["sourceSets"] as SourceSetContainer
                 val kotlinSourceSet = (sourceSets.getByName("main") as HasConvention).convention.getPlugin(KotlinSourceSet::class.java).kotlin
@@ -48,29 +49,29 @@ class LimitsPlugin: Plugin<Project> {
                 specs.forEach { spec ->
                     if (spec is ContainerSpec) {
                         if (spec.containedType.projectName() != spec.projectName && !spec.containedType.projectName().isEmpty()) {
-                            val projectDependency = sub.dependencies.project(mapOf("path" to ":${spec.containedType.projectName()}"))
+                            val projectDependency = sub.dependencies.project(":${spec.containedType.projectName()}")
                             sub.dependencies.add("compile", projectDependency)
                         }
                         if (spec.coordinatesType.projectName() != spec.projectName && !spec.coordinatesType.projectName().isEmpty()) {
-                            val projectDependency = sub.dependencies.project(mapOf("path" to ":${spec.coordinatesType.projectName()}"))
+                            val projectDependency = sub.dependencies.project(":${spec.coordinatesType.projectName()}")
                             sub.dependencies.add("compile", projectDependency)
                         }
                         spec.additionalContainedTypes.forEach {
                             if (it.projectName() != spec.projectName && !it.projectName().isEmpty()) {
-                                val projectDependency = sub.dependencies.project(mapOf("path" to ":${it.projectName()}"))
+                                val projectDependency = sub.dependencies.project(":${it.projectName()}")
                                 sub.dependencies.add("compile", projectDependency)
                             }
                         }
                         spec.attributes.forEach {
                             if (it.projectName() != spec.projectName && !it.projectName().isEmpty()) {
-                                val projectDependency = sub.dependencies.project(mapOf("path" to ":${it.projectName()}"))
+                                val projectDependency = sub.dependencies.project(":${it.projectName()}")
                                 sub.dependencies.add("compile", projectDependency)
                             }
                         }
                     } else if (spec is ChainOfCoordinates) {
                         spec.components.forEach {
                             if (it.projectName() != spec.projectName && !it.projectName().isEmpty()) {
-                                val projectDependency = sub.dependencies.project(mapOf("path" to ":${it.projectName()}"))
+                                val projectDependency = sub.dependencies.project(":${it.projectName()}")
                                 sub.dependencies.add("compile", projectDependency)
                             }
                         }
