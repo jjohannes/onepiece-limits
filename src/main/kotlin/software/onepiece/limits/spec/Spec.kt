c863dbf040af0a17e6c3416f0f71c43dd9222d7d
Jendrik Johannes <jendrik@gradle.com>
2019-02-13 09:36:57 +0100

    Default value checks for all types

--- src/main/kotlin/software/onepiece/limits/spec/Spec.kt
+++ src/main/kotlin/software/onepiece/limits/spec/Spec.kt
@@ -6,6 +6,7 @@ interface Spec : Serializable {
     fun projectName(): String
     fun typeName(): String
     fun generateEmpty(): String
+    fun emptyCheck(): String
     fun propertyName(count: Int = 0) = typeName().decapitalize() + if (count == 0) "" else count.toString()
     fun generate(packageName: String) = ""
     fun generateCommandFactory(packageName: String) = ""
--- src/main/kotlin/software/onepiece/limits/spec/SpecReference.kt
+++ src/main/kotlin/software/onepiece/limits/spec/SpecReference.kt
@@ -1,16 +1,8 @@
 package software.onepiece.limits.spec
 
 class SpecReference(val typeName: String) : Spec {
-    override fun generateEmpty(): String {
-        throw RuntimeException("Spec reference needs to be resolved!")
-    }
-
-    override fun projectName(): String {
-        throw RuntimeException("Spec reference needs to be resolved!")
-    }
-
-    override fun typeName(): String {
-        throw RuntimeException("Spec reference needs to be resolved!")
-    }
-
+    override fun generateEmpty() = throw RuntimeException("Spec reference needs to be resolved!")
+    override fun emptyCheck() = throw RuntimeException("Spec reference needs to be resolved!")
+    override fun projectName() = throw RuntimeException("Spec reference needs to be resolved!")
+    override fun typeName() = throw RuntimeException("Spec reference needs to be resolved!")
 }
