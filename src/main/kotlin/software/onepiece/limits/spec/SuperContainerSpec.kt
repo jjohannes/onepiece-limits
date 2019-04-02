dda975257d8a2e6830635094f1a98583369cadf7
Jendrik Johannes <jendrik@gradle.com>
2019-04-02 22:45:48 +0200

    Fixed bug with subtypes+diff

--- src/main/kotlin/software/onepiece/limits/spec/SuperContainerSpec.kt
+++ src/main/kotlin/software/onepiece/limits/spec/SuperContainerSpec.kt
@@ -1,16 +1,20 @@
 package software.onepiece.limits.spec
 
-class SuperContainerSpec(val projectName: String, val typeName: String, val containedType: Spec) : Spec {
+class SuperContainerSpec(val projectName: String, val typeName: String) : Spec {
     override fun projectName() = projectName
     override fun typeName() = typeName
 
-    override fun generateEmpty() = "throw IllegalArgumentException()"
+    override fun generateEmpty() = "$typeName.empty"
     override fun emptyCheck() = ".isEmpty()"
     override fun generate(packageName: String) = """
         package $packageName.entities.$projectName
 
         interface $typeName {
             fun dataHash() = hashCode()
+
+            companion object {
+                val empty = object: $typeName {}
+            }
         }
     """.trimIndent()
 }
